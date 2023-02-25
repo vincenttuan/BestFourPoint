767370d8ee5eb62a94f588df95af3743c18f2d9a
MB-teacher <vincentjava@yahoo.com.tw>
2023-02-25 16:46:37 +0800

    bfp

--- api.py
+++ api.py
@@ -60,5 +60,7 @@ def qyery_stocks():
 
 
 if __name__ == '__main__':
-    uvicorn.run('api:app')
+    # uvicorn.run('api:app')
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, log_level="info")
+
 
